e9bf4a3e47979fe170e537884de2c4fb3e33d46a
SohamGhugare <soham.ghugare@gmail.com>
2023-04-12 20:47:46 +0530

    Added frontend skeleton

---
+++ chatroom-async/frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>ChatRoom</h1>
+      <h2>Your Client ID</h2>
+      <div className="chat-container">
+        <div className="chat">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
