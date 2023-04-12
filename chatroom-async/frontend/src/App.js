2789ce326b48024b32012546533e9a4d4f34f579
SohamGhugare <soham.ghugare@gmail.com>
2023-04-12 21:21:22 +0530

    Added client id

--- chatroom-async/frontend/src/App.js
+++ chatroom-async/frontend/src/App.js
@@ -4,9 +4,7 @@ import React, { useState, useEffect } from 'react';
 function App() {
   // Using current datetime for client id
   const [clientId, setClientId] = useState(Math.floor(new Date().getTime()/1000))
-  const [chatHistory, setChatHistory] = useState([])
-  const [isOnline, setIsOnline] = useState(false)
-  const [textValue, setTextValue] = useState("")
+
   const [websoc, setWebsoc] = useState()
   const [message, setMessage] = useState([])
   const [messages, setMessages] = useState([])
@@ -45,24 +43,28 @@ function App() {
   return (
     <div className="container">
       <h1>ChatRoom</h1>
-      <h2>Your Client ID</h2>
+      <h2>Your Client ID: {clientId}</h2>
       <div className="chat-container">
         <div className="chat">
           {messages.map((value, index) => {
             if(value.clientId === clientId){
-              <div className="messages-container">
-                <div className="messages">
-                  <p className="client">Client ID: </p>
-                  <p className="message">Hello World</p>
+              return (    
+                <div className="messages-container">
+                  <div className="messages">
+                    <p className="client">Client ID: </p>
+                    <p className="message">Hello World</p>
+                  </div>
                 </div>
-              </div>
+              )
             } else {
-              <div className="messages-container-2">
-                <div className="messages-2">
-                  <p className="client">Client ID: </p>
-                  <p className="message">Hello World</p>
+              return (
+                <div className="messages-container-2">
+                  <div className="messages-2">
+                    <p className="client">Client ID: </p>
+                    <p className="message">Hello World</p>
+                  </div>
                 </div>
-              </div>
+              )
             }
           })}
         </div>
